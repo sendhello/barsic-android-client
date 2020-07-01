12e0d0acb989347f2dc1740f74f878941f8a38e6
Ivan Bazhenov <ivan.bazhenov@balance-pl.ru>
2020-07-01 20:36:33 +0300

    Изменение мониторинга ошибок с встроенного на sentry

--- main.py
+++ main.py
@@ -3,14 +3,30 @@
 import os
 import sys
 import traceback
+import platform
 
-NICK_NAME_AND_NAME_REPOSITORY = 'sendhello/barsic-android-client'
+from sentry_sdk import init as senry_init, push_scope, capture_exception
+senry_init('https://9e367e10d7a24038942dc7d20db5767c@o412552.ingest.sentry.io/5304929')
+
+
+def send_sentry(error):
+    with push_scope() as scope:
+        scope.set_context('context', {
+            'platform': platform.system(),
+            'platform releas': platform.release(),
+            'platform version': platform.version(),
+            'module': 'barsic_android_client',
+            'version': __version__
+        })
+        scope.set_tag('module', 'barsic_android_client')
+        capture_exception(error)
+        raise error
+
 
 directory = os.path.split(os.path.abspath(sys.argv[0]))[0]
-sys.path.insert(0, os.path.join(directory, 'libs/applibs'))
+sys.path.insert(0, os.path.join(directory, 'applibs'))
 
 try:
-    import webbrowser
     try:
         import six.moves.urllib
     except ImportError:
@@ -23,76 +39,23 @@ try:
     Config.set('kivy', 'keyboard_mode', 'system')
     Config.set('kivy', 'log_enable', 0)
 
-    from kivymd.theming import ThemeManager
-    from applibs.bugreporter.bugreporter import BugReporter
-except Exception:
+except Exception as e:
     traceback.print_exc(file=open(os.path.join(directory, 'error.log'), 'w'))
-    print(traceback.print_exc())
-    sys.exit(1)
+    send_sentry(e)
 
 
 __version__ = '3.0.0.0'
 
 
 def main():
-    def create_error_monitor():
-        class _App(App):
-            theme_cls = ThemeManager()
-            theme_cls.primary_palette = 'BlueGray'
-
-            def build(self):
-                box = BoxLayout()
-                box.add_widget(report)
-                return box
-        app = _App()
-        app.run()
-
-    app = None
-
     try:
         from barsic import Barsic
 
         app = Barsic()
         app.run()
-    except Exception:
-        from kivy.app import App
-        from kivy.uix.boxlayout import BoxLayout
-
-
-        text_error = traceback.format_exc()
+    except Exception as e:
         traceback.print_exc(file=open(os.path.join(directory, 'error.log'), 'w'))
-
-        # if app:
-        #     try:
-        #         app.stop()
-        #     except AttributeError:
-        #         app = None
-
-        def callback_report(*args):
-            '''Функция отправки баг-репорта.'''
-
-            try:
-                txt = six.moves.urllib.parse.quote(
-                    report.txt_traceback.text.encode('utf-8')
-                )
-                url = 'https://github.com/%s/issues/new?body=' % NICK_NAME_AND_NAME_REPOSITORY + txt
-                webbrowser.open(url)
-            except Exception:
-                sys.exit(1)
-
-        report = BugReporter(
-            callback_report=callback_report, txt_report=text_error,
-            icon_background=os.path.join('data', 'images', 'icon.png')
-        )
-
-        if app:
-            try:
-                app.screen.ids.manager.ids.base.clear_widgets()
-                app.screen.ids.manager.ids.base.add_widget(report)
-            except AttributeError:
-                create_error_monitor()
-        else:
-            create_error_monitor()
+        send_sentry(e)
 
 
 if __name__ in ('__main__', '__android__'):
